d6719b39c699a38d1d113c6d88aba0f4fd846027
Travis CI User <travis@example.org>
2018-09-14 12:41:20 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-46a7f7a42a04712b6b5a.js","485d0349815a59d31aa5f4ac2732bd0d"],["/commons-2995faa554de89ed1fce.js","251adbb1faa46d0d978f53b853742104"],["/index.html","9de6615070280f0723dc4f1f5efdc383"],["/manifest.json","a1153fda830c2f1f6f502c35b0e09834"],["/offline-plugin-app-shell-fallback/index.html","5cfee3d91ef26e0de45bf9ce145f0f37"]];
+var precacheConfig = [["/app-46a7f7a42a04712b6b5a.js","485d0349815a59d31aa5f4ac2732bd0d"],["/commons-2995faa554de89ed1fce.js","251adbb1faa46d0d978f53b853742104"],["/index.html","cf2b6b0a5a58d4923d8c3a615d12d52c"],["/manifest.json","a1153fda830c2f1f6f502c35b0e09834"],["/offline-plugin-app-shell-fallback/index.html","61f2a1c4352c34b6320395c34d8c7a69"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
