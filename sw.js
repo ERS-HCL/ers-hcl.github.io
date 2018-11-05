c33f87088f111bab63cdfc817451a3af14bc5900
Travis CI User <travis@example.org>
2018-11-05 05:58:47 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-00d53e0e965914f746be.js"
+    "url": "webpack-runtime-1f3d8dea89c0f6cc43f8.js"
   },
   {
     "url": "app-306caa533bc7649d905d.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "afbdc69ac5b44a2ccc60e17dd2a9a1c4"
+    "revision": "24b1af36f7a07d7e2aff99e440415fee"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "52f058ea0297a7574ce14f5820bae734"
+    "revision": "6a4a47b3a84e1a68906efadd4e0b8220"
   },
   {
     "url": "3.532791e79fc0bb23dc60.css"
@@ -61,8 +61,8 @@ self.__precacheManifest = [
     "url": "2-16723b8fa1cb7075ba5c.js"
   },
   {
-    "url": "static/d/927/path---index-6a9-RMbywqgVRpS8RU8feGhyKajlmQw.json",
-    "revision": "617a524f66cb10edb73b511e8066ca6a"
+    "url": "static/d/378/path---index-6a9-KTSfWqoQDVzV1W8msOumyOktzxE.json",
+    "revision": "6975a4c69b0d1b6c885bef0e8d8f9273"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
