93e7d712bf6b5a0e8146b85c93066528e0b8f0f6
Travis CI User <travis@example.org>
2019-03-25 14:26:03 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f1814170574decb0a32.js"
+    "url": "webpack-runtime-ed83bfc1268431088245.js"
   },
   {
     "url": "styles.c57d3fca6ea8b6610e88.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3d62c86b9a23910fefe2db4583e2b87d"
+    "revision": "29e1f61dace7fed7d8705e9840c021ec"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
