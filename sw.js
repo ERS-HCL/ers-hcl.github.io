fc7be2a7d6fa6e0f05281a1c4a04c60d8dac2403
Travis CI User <travis@example.org>
2019-10-07 16:13:20 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-357c67922a802d3ae8d0.js"
+    "url": "webpack-runtime-f5113a33da42f419f63f.js"
   },
   {
     "url": "styles.df32eba535376af6b243.css"
@@ -35,14 +35,14 @@ self.__precacheManifest = [
     "url": "styles-fe9ac1d5f1470326fd12.js"
   },
   {
-    "url": "app-79a8bcd855c98d1952cd.js"
+    "url": "app-2d5fa65410dedaa6bab0.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-24f81d0f7591ce023728.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f02df11c95e31d6923de4f0e3669f5cb"
+    "revision": "015e108d23d1f432c03724d9eed3dd3a"
   },
   {
     "url": "google-fonts/s/montserrat/v14/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2",
