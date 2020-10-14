2f571248c29bcf62d0780c6979f2fcdd5761f0ae
Travis CI User <travis@example.org>
2020-10-14 19:23:01 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ae804ec45d7320dc5652.js"
+    "url": "webpack-runtime-296fc240e76255bd4e53.js"
   },
   {
     "url": "styles.df32eba535376af6b243.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "69d245999f33a02825c4a0b32409f38d"
+    "revision": "035a48bbb9267b1eb84a934b76741725"
   },
   {
     "url": "google-fonts/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2",
